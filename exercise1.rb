2da4e221ab0aa6f45eec36cd3f7c1946d8dff30d
chanashuster <chana.shuster@gmail.com>
2018-12-18 21:45:18 -0500

    exercises1-5

--- exercise1.rb
+++ exercise1.rb
@@ -1,6 +1,60 @@
-def double(my_number)
-    return my_number*2
-end
+#EXERCISE ONE
 
-puts double(3)
-puts double(7)
+#def double(my_number)
+#    return my_number*2
+#end
+
+#puts double(3)
+#puts double(7)
+
+
+#EXERCISE TWO
+
+#    def negative?(num)
+#   num>0
+#    end
+    
+#    puts negative?(2)
+#    puts negative?(-9)
+
+
+
+# EXERCISE THREE
+
+# def is_even?(x)
+#    x%2==0
+# end
+
+# puts is_even?(4)
+# puts is_even?(9)
+
+
+#EXERCISE 4
+
+# def longword(word)
+#     if word.length>=8
+#         return true
+#     else
+#         return false
+#     end
+# end
+
+# puts longword("hey")
+# puts longword("hellohello")
+
+
+#EXERCISE FIVE
+
+# def greet_backwards(name)
+#     puts "Hello #{name.reverse*2} welcome home"
+    
+# end 
+
+# print greet_backwards("Bob")
+# print greet_backwards("Shirley")
+# print greet_backwards("Sue")
+# print greet_backwards("Andy")
+
+
+#EXERCISE 6
+
